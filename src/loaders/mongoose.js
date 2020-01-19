052b6bfd02be1c459eb4b061d662376b12fd04dd
Kevin Caicedo <ing.sys.kevincaicedo@gmail.com>
2020-01-19 00:53:33 -0500

    format lint

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -4,21 +4,23 @@ import Chalk from 'chalk'
 
 const { DB_USER, DB_PASSWD, DB_HOST, DB_PORT } = process.env
 const mongoUrl = `mongodb://${DB_USER}:${DB_PASSWD}@${DB_HOST}:${DB_PORT}`
-let dbConnection
 
 export default async () => {
-  if(dbConnection) return dbConnection  
-  dbConnection = await connect(mongoUrl, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
+  const dbConnection = await connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true
   })
 
-  dbConnection.connection.on('connected', (_) => console.log(Chalk.green('Connected')))
+  dbConnection.connection.on('connected', _ =>
+    console.log(Chalk.green('Connected'))
+  )
 
   dbConnection.connection.on('error', err => {
-    console.log(Chalk.bgRed('Mongoose default connection has occured ' + err + ' error'))
+    console.log(
+      Chalk.bgRed('Mongoose default connection has occured ' + err + ' error')
+    )
   })
 
   dbConnection.connection.on('disconnected', () => {
